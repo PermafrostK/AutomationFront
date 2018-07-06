f805009e4fd85fab097de2b913d8e39ea7d11e45
Virus <krodriguezhub310@gmail.com>
2018-07-05 23:41:51 -0500

    Create Footer + Content + Header

---
+++ src/Components/Global/Content.js
@@ -0,0 +1,18 @@
+//Dependencies
+import React, { Component } from 'react';
+
+//Assets
+import './css/Content.css';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="Content">
+                <h1>Contenido</h1>
+                <p>Lorem Ipsu Dolor....</p>
+            </div>
+        );
+    }
+}
+
+export default Content;
---
+++ src/Components/Global/Footer.js
@@ -0,0 +1,17 @@
+//Dependencies
+import React, { Component } from 'react';
+
+//Assets
+import './css/Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="Footer">
+                <h5>Create By Kevin Rodriguez</h5>
+            </div>
+        );
+    }
+}
+
+export default Footer;
